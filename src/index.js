6b496ae4fd765f2356d3b6e9447b8fe651dd5f8d
Lautaro <lautarosantillann@icloud.com>
2022-11-15 17:41:44 -0300

    Enviroment Port Fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import app from './app.js';
 import './database.js';
-app.listen(8080)
-console.log('Server is listening on port', 8080);
+const PORT = process.env.PORT || 5001
+app.listen(PORT)
+console.log('Server is listening on port', PORT);
