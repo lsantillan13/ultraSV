00a8d939484e72fdec5c9318412c31a1843b2a8c
lautaro santillan <lautisantillann@hotmail.com>
2024-03-23 12:57:15 -0300

    fifth

--- build/app.js
+++ build/app.js
@@ -5,13 +5,13 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 var _express = _interopRequireDefault(require("express"));
+var _initialSetup = require("./libs/initialSetup.js");
 var _morgan = _interopRequireDefault(require("morgan"));
 var _postRoutes = _interopRequireDefault(require("./routes/post.routes.js"));
 var _authRoutes = _interopRequireDefault(require("./routes/auth.routes.js"));
 var _userRoutes = _interopRequireDefault(require("./routes/user.routes.js"));
 var _corteRoutes = _interopRequireDefault(require("./routes/corte.routes.js"));
 var _cors = _interopRequireDefault(require("cors"));
-var _initialSetup = require("./libs/initialSetup.js");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var app = (0, _express["default"])();
 (0, _initialSetup.createRoles)();
