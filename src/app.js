d912c47c9db03838c20f5372065480da2d0192a8
lautaro santillan <lautisantillann@hotmail.com>
2024-04-04 12:37:30 -0300

    typo fix

--- src/app.js
+++ src/app.js
@@ -11,22 +11,15 @@ const app = express();
 
 
 createRoles();
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'));
-app.use(cors());
-app.use(function (req, res, next) {
-  const allowedOrigins = ['https://voxdiario.com', 'http://127.0.0.1:5001', 'http://localhost:5001', 'https://ultravox.netlify.app', 'http://localhost:3000'];
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin); 
-  }
-
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Otros encabezados CORS opcionales
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
