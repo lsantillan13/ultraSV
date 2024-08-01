c13691bcc2e36ee417bd967165da95a3ba44093f
Lautaro Santillán <81720647+lsantillan13@users.noreply.github.com>
2024-08-01 09:34:41 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './database.js';
 import app from './app.js';
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 80
 app.listen(PORT)
 console.log('Server is listening on port', PORT);
