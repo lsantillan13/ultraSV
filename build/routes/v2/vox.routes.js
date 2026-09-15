bed7eabf68fc86cd2bc5363a4866f427f7523bd1
lsantillan13 <lautarosantillann@icloud.com>
2026-09-15 20:11:19 -0300

    add groq

--- build/routes/v2/vox.routes.js
+++ build/routes/v2/vox.routes.js
@@ -5,6 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 var _express = _interopRequireDefault(require("express"));
+var _axios = _interopRequireDefault(require("axios"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 function _regenerator() { /*! regenerator-runtime -- Copyright (c) 2014-present, Facebook, Inc. -- license (MIT): https://github.com/babel/babel/blob/main/packages/babel-helpers/LICENSE */ var e, t, r = "function" == typeof Symbol ? Symbol : {}, n = r.iterator || "@@iterator", o = r.toStringTag || "@@toStringTag"; function i(r, n, o, i) { var c = n && n.prototype instanceof Generator ? n : Generator, u = Object.create(c.prototype); return _regeneratorDefine2(u, "_invoke", function (r, n, o) { var i, c, u, f = 0, p = o || [], y = !1, G = { p: 0, n: 0, v: e, a: d, f: d.bind(e, 4), d: function d(t, r) { return i = t, c = 0, u = e, G.n = r, a; } }; function d(r, n) { for (c = r, u = n, t = 0; !y && f && !o && t < p.length; t++) { var o, i = p[t], d = G.p, l = i[2]; r > 3 ? (o = l === n) && (u = i[(c = i[4]) ? 5 : (c = 3, 3)], i[4] = i[5] = e) : i[0] <= d && ((o = r < 2 && d < i[1]) ? (c = 0, G.v = n, G.n = i[1]) : d < l && (o = r < 3 || i[0] > n || n > l) && (i[4] = r, i[5] = n, G.n = l, c = 0)); } if (o || r > 1) return a; throw y = !0, n; } return function (o, p, l) { if (f > 1) throw TypeError("Generator is already running"); for (y && 1 === p && d(p, l), c = p, u = l; (t = c < 2 ? e : u) || !y;) { i || (c ? c < 3 ? (c > 1 && (G.n = -1), d(c, u)) : G.n = u : G.v = u); try { if (f = 2, i) { if (c || (o = "next"), t = i[o]) { if (!(t = t.call(i, u))) throw TypeError("iterator result is not an object"); if (!t.done) return t; u = t.value, c < 2 && (c = 0); } else 1 === c && (t = i["return"]) && t.call(i), c < 2 && (u = TypeError("The iterator does not provide a '" + o + "' method"), c = 1); i = e; } else if ((t = (y = G.n < 0) ? u : r.call(n, G)) !== a) break; } catch (t) { i = e, c = 1, u = t; } finally { f = 1; } } return { value: t, done: y }; }; }(r, o, i), !0), u; } var a = {}; function Generator() {} function GeneratorFunction() {} function GeneratorFunctionPrototype() {} t = Object.getPrototypeOf; var c = [][n] ? t(t([][n]())) : (_regeneratorDefine2(t = {}, n, function () { return this; }), t), u = GeneratorFunctionPrototype.prototype = Generator.prototype = Object.create(c); function f(e) { return Object.setPrototypeOf ? Object.setPrototypeOf(e, GeneratorFunctionPrototype) : (e.__proto__ = GeneratorFunctionPrototype, _regeneratorDefine2(e, o, "GeneratorFunction")), e.prototype = Object.create(u), e; } return GeneratorFunction.prototype = GeneratorFunctionPrototype, _regeneratorDefine2(u, "constructor", GeneratorFunctionPrototype), _regeneratorDefine2(GeneratorFunctionPrototype, "constructor", GeneratorFunction), GeneratorFunction.displayName = "GeneratorFunction", _regeneratorDefine2(GeneratorFunctionPrototype, o, "GeneratorFunction"), _regeneratorDefine2(u), _regeneratorDefine2(u, o, "Generator"), _regeneratorDefine2(u, n, function () { return this; }), _regeneratorDefine2(u, "toString", function () { return "[object Generator]"; }), (_regenerator = function _regenerator() { return { w: i, m: f }; })(); }
 function _regeneratorDefine2(e, r, n, t) { var i = Object.defineProperty; try { i({}, "", {}); } catch (e) { i = 0; } _regeneratorDefine2 = function _regeneratorDefine(e, r, n, t) { function o(r, n) { _regeneratorDefine2(e, r, function (e) { return this._invoke(r, n, e); }); } r ? i ? i(e, r, { value: n, enumerable: !t, configurable: !t, writable: !t }) : e[r] = n : (o("next", 0), o("throw", 1), o("return", 2)); }, _regeneratorDefine2(e, r, n, t); }
@@ -13,11 +14,17 @@ function _asyncToGenerator(n) { return function () { var t = this, e = arguments
 var router = _express["default"].Router();
 router.post('/vox-rewrite', /*#__PURE__*/function () {
   var _ref = _asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee(req, res) {
-    var _req$body, texto_original, fuente1, modo, prompt, r, j, data, _t;
+    var _req$body, texto_original, fuente1, modo, prompt, r, data, _e$response, _e$response2, _t;
     return _regenerator().w(function (_context) {
       while (1) switch (_context.p = _context.n) {
         case 0:
           _req$body = req.body, texto_original = _req$body.texto_original, fuente1 = _req$body.fuente1, modo = _req$body.modo;
+          console.log("LLEGÓ REQUEST", {
+            largo: texto_original === null || texto_original === void 0 ? void 0 : texto_original.length,
+            fuente1: fuente1,
+            modo: modo,
+            tieneKey: !!process.env.GROQ_API_KEY
+          });
           if (texto_original) {
             _context.n = 1;
             break;
@@ -26,49 +33,48 @@ router.post('/vox-rewrite', /*#__PURE__*/function () {
             error: "Falta texto_original"
           }));
         case 1:
-          prompt = "\nSos editor de VoxDiario Neuqu\xE9n.\nModo: ".concat(modo, "\nFuente: ").concat(fuente1, "\nReescrib\xED 100% original, manteniendo todos los datos. No copies m\xE1s de 4 palabras seguidas.\nDevolv\xE9 SOLO JSON: { \"Entry_Title\": \"\", \"Entry_Bajada\": \"\", \"Entry_Content\": \"\", \"Entry_Slug\": \"\", \"Entry_Category\": \"general\" }\nTexto: ").concat(texto_original, "\n");
+          prompt = "Sos editor de VoxDiario Neuqu\xE9n. Modo: ".concat(modo, " Fuente: ").concat(fuente1, ". Reescrib\xED 100% original. Devolv\xE9 SOLO JSON: { \"Entry_Title\": \"\", \"Entry_Bajada\": \"\", \"Entry_Content\": \"\", \"Entry_Slug\": \"\", \"Entry_Category\": \"general\" } Texto: ").concat(texto_original);
           _context.p = 2;
           _context.n = 3;
-          return fetch("https://api.groq.com/openai/v1/chat/completions", {
-            method: "POST",
+          return _axios["default"].post("https://api.groq.com/openai/v1/chat/completions", {
+            model: "llama-3.1-8b-instant",
+            messages: [{
+              role: "user",
+              content: prompt
+            }],
+            temperature: 0.6,
+            response_format: {
+              type: "json_object"
+            }
+          }, {
             headers: {
               "Content-Type": "application/json",
               "Authorization": "Bearer ".concat(process.env.GROQ_API_KEY)
             },
-            body: JSON.stringify({
-              model: "llama-3.3-70b-versatile",
-              messages: [{
-                role: "user",
-                content: prompt
-              }],
-              temperature: 0.6,
-              response_format: {
-                type: "json_object"
-              }
-            })
+            timeout: 30000
           });
         case 3:
           r = _context.v;
-          _context.n = 4;
-          return r.json();
-        case 4:
-          j = _context.v;
-          data = JSON.parse(j.choices[0].message.content);
+          console.log("RESPUESTA GROQ OK:", r.data.choices[0].message.content.slice(0, 100));
+          data = JSON.parse(r.data.choices[0].message.content);
           res.json({
             data: data
           });
-          _context.n = 6;
+          _context.n = 5;
           break;
-        case 5:
-          _context.p = 5;
+        case 4:
+          _context.p = 4;
           _t = _context.v;
+          console.log("ERROR GROQ COMPLETO:", ((_e$response = _t.response) === null || _e$response === void 0 ? void 0 : _e$response.data) || _t.message);
           res.status(500).json({
-            error: _t.message
+            error: "Groq falló",
+            detalle: ((_e$response2 = _t.response) === null || _e$response2 === void 0 ? void 0 : _e$response2.data) || _t.message,
+            keyExiste: !!process.env.GROQ_API_KEY
           });
-        case 6:
+        case 5:
           return _context.a(2);
       }
-    }, _callee, null, [[2, 5]]);
+    }, _callee, null, [[2, 4]]);
   }));
   return function (_x, _x2) {
     return _ref.apply(this, arguments);
--- src/routes/v2/vox.routes.js
+++ src/routes/v2/vox.routes.js
@@ -1,38 +1,40 @@
 import express from "express"
+import axios from "axios"
 const router = express.Router()
 
 router.post('/vox-rewrite', async (req, res) => {
   const { texto_original, fuente1, modo } = req.body
+  console.log("LLEGÓ REQUEST", { largo: texto_original?.length, fuente1, modo, tieneKey:!!process.env.GROQ_API_KEY })
+
   if (!texto_original) return res.status(400).json({ error: "Falta texto_original" })
 
-  const prompt = `
-Sos editor de VoxDiario Neuquén.
-Modo: ${modo}
-Fuente: ${fuente1}
-Reescribí 100% original, manteniendo todos los datos. No copies más de 4 palabras seguidas.
-Devolvé SOLO JSON: { "Entry_Title": "", "Entry_Bajada": "", "Entry_Content": "", "Entry_Slug": "", "Entry_Category": "general" }
-Texto: ${texto_original}
-`
+  const prompt = `Sos editor de VoxDiario Neuquén. Modo: ${modo} Fuente: ${fuente1}. Reescribí 100% original. Devolvé SOLO JSON: { "Entry_Title": "", "Entry_Bajada": "", "Entry_Content": "", "Entry_Slug": "", "Entry_Category": "general" } Texto: ${texto_original}`
 
   try {
-    const r = await fetch("https://api.groq.com/openai/v1/chat/completions", {
-      method: "POST",
+    const r = await axios.post("https://api.groq.com/openai/v1/chat/completions", {
+      model: "llama-3.1-8b-instant",
+      messages: [{ role: "user", content: prompt }],
+      temperature: 0.6,
+      response_format: { type: "json_object" }
+    }, {
       headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
       },
-      body: JSON.stringify({
-        model: "llama-3.3-70b-versatile",
-        messages: [{ role: "user", content: prompt }],
-        temperature: 0.6,
-        response_format: { type: "json_object" }
-      })
+      timeout: 30000
     })
-    const j = await r.json()
-    const data = JSON.parse(j.choices[0].message.content)
+
+    console.log("RESPUESTA GROQ OK:", r.data.choices[0].message.content.slice(0,100))
+    const data = JSON.parse(r.data.choices[0].message.content)
     res.json({ data })
+
   } catch (e) {
-    res.status(500).json({ error: e.message })
+    console.log("ERROR GROQ COMPLETO:", e.response?.data || e.message)
+    res.status(500).json({
+      error: "Groq falló",
+      detalle: e.response?.data || e.message,
+      keyExiste:!!process.env.GROQ_API_KEY
+    })
   }
 })
 
