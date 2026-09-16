fdc4e84ba2c1b525676fc31c15c028aa90aff728
lsantillan13 <lautarosantillann@icloud.com>
2026-09-16 15:40:07 -0300

    add fixed

--- build/routes/v2/vox-rewrite.routes.js
+++ build/routes/v2/vox-rewrite.routes.js
@@ -1,6 +1,5 @@
 "use strict";
 
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -10,26 +9,26 @@ var _axios = _interopRequireDefault(require("axios"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 function _regenerator() { /*! regenerator-runtime -- Copyright (c) 2014-present, Facebook, Inc. -- license (MIT): https://github.com/babel/babel/blob/main/packages/babel-helpers/LICENSE */ var e, t, r = "function" == typeof Symbol ? Symbol : {}, n = r.iterator || "@@iterator", o = r.toStringTag || "@@toStringTag"; function i(r, n, o, i) { var c = n && n.prototype instanceof Generator ? n : Generator, u = Object.create(c.prototype); return _regeneratorDefine2(u, "_invoke", function (r, n, o) { var i, c, u, f = 0, p = o || [], y = !1, G = { p: 0, n: 0, v: e, a: d, f: d.bind(e, 4), d: function d(t, r) { return i = t, c = 0, u = e, G.n = r, a; } }; function d(r, n) { for (c = r, u = n, t = 0; !y && f && !o && t < p.length; t++) { var o, i = p[t], d = G.p, l = i[2]; r > 3 ? (o = l === n) && (u = i[(c = i[4]) ? 5 : (c = 3, 3)], i[4] = i[5] = e) : i[0] <= d && ((o = r < 2 && d < i[1]) ? (c = 0, G.v = n, G.n = i[1]) : d < l && (o = r < 3 || i[0] > n || n > l) && (i[4] = r, i[5] = n, G.n = l, c = 0)); } if (o || r > 1) return a; throw y = !0, n; } return function (o, p, l) { if (f > 1) throw TypeError("Generator is already running"); for (y && 1 === p && d(p, l), c = p, u = l; (t = c < 2 ? e : u) || !y;) { i || (c ? c < 3 ? (c > 1 && (G.n = -1), d(c, u)) : G.n = u : G.v = u); try { if (f = 2, i) { if (c || (o = "next"), t = i[o]) { if (!(t = t.call(i, u))) throw TypeError("iterator result is not an object"); if (!t.done) return t; u = t.value, c < 2 && (c = 0); } else 1 === c && (t = i["return"]) && t.call(i), c < 2 && (u = TypeError("The iterator does not provide a '" + o + "' method"), c = 1); i = e; } else if ((t = (y = G.n < 0) ? u : r.call(n, G)) !== a) break; } catch (t) { i = e, c = 1, u = t; } finally { f = 1; } } return { value: t, done: y }; }; }(r, o, i), !0), u; } var a = {}; function Generator() {} function GeneratorFunction() {} function GeneratorFunctionPrototype() {} t = Object.getPrototypeOf; var c = [][n] ? t(t([][n]())) : (_regeneratorDefine2(t = {}, n, function () { return this; }), t), u = GeneratorFunctionPrototype.prototype = Generator.prototype = Object.create(c); function f(e) { return Object.setPrototypeOf ? Object.setPrototypeOf(e, GeneratorFunctionPrototype) : (e.__proto__ = GeneratorFunctionPrototype, _regeneratorDefine2(e, o, "GeneratorFunction")), e.prototype = Object.create(u), e; } return GeneratorFunction.prototype = GeneratorFunctionPrototype, _regeneratorDefine2(u, "constructor", GeneratorFunctionPrototype), _regeneratorDefine2(GeneratorFunctionPrototype, "constructor", GeneratorFunction), GeneratorFunction.displayName = "GeneratorFunction", _regeneratorDefine2(GeneratorFunctionPrototype, o, "GeneratorFunction"), _regeneratorDefine2(u), _regeneratorDefine2(u, o, "Generator"), _regeneratorDefine2(u, n, function () { return this; }), _regeneratorDefine2(u, "toString", function () { return "[object Generator]"; }), (_regenerator = function _regenerator() { return { w: i, m: f }; })(); }
 function _regeneratorDefine2(e, r, n, t) { var i = Object.defineProperty; try { i({}, "", {}); } catch (e) { i = 0; } _regeneratorDefine2 = function _regeneratorDefine(e, r, n, t) { function o(r, n) { _regeneratorDefine2(e, r, function (e) { return this._invoke(r, n, e); }); } r ? i ? i(e, r, { value: n, enumerable: !t, configurable: !t, writable: !t }) : e[r] = n : (o("next", 0), o("throw", 1), o("return", 2)); }, _regeneratorDefine2(e, r, n, t); }
-function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
-function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
-function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
-function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 function asyncGeneratorStep(n, t, e, r, o, a, c) { try { var i = n[a](c), u = i.value; } catch (n) { return void e(n); } i.done ? t(u) : Promise.resolve(u).then(r, o); }
 function _asyncToGenerator(n) { return function () { var t = this, e = arguments; return new Promise(function (r, o) { var a = n.apply(t, e); function _next(n) { asyncGeneratorStep(a, r, o, _next, _throw, "next", n); } function _throw(n) { asyncGeneratorStep(a, r, o, _next, _throw, "throw", n); } _next(void 0); }); }; }
 var router = _express["default"].Router();
-var SYSTEM_PROMPT = "\nSos EDITOR JEFE de un diario digital argentino.\n\nTAREA: Reescribir la noticia que te dan, manteniendo TODA la informaci\xF3n.\n\nREGLAS OBLIGATORIAS:\n1. LARGO PROPORCIONAL: Cont\xE1 las palabras del texto original. Devolv\xE9 la MISMA cantidad de palabras o un poco m\xE1s (90% a 110%). Si el original tiene 200 palabras, devolv\xE9s 200. Si tiene 800, devolv\xE9s 800. NUNCA resumas.\n2. PROHIBIDO INVENTAR: No inventes fechas, a\xF1os, nombres, montos, lugares, cargos, estad\xEDsticas. Copi\xE1 exactamente lo que dice el texto original. Si dice \"fin de a\xF1o\", dej\xE1 \"fin de a\xF1o\". No agregues el a\xF1o si no est\xE1.\n3. CONSERVAR TODO: Manten\xE9 todas las declaraciones textuales, todos los nombres propios, todos los n\xFAmeros, todas las calles/barrios/organismos.\n4. FORMATO: Us\xE1 HTML para contenido_mejorado: <p> para p\xE1rrafos, <h2> para cada subt\xEDtulo que ya exista en el original, <strong> para datos clave. 5 a 10 p\xE1rrafos seg\xFAn el largo original.\n5. Tono: Profesional, objetivo, argentino.\n\nDevolv\xE9 SOLO JSON v\xE1lido:\n{\"titulo\":\"60-75 caracteres, atractivo\",\"bajada\":\"150-180 caracteres\",\"contenido_mejorado\":\"HTML largo y proporcional\",\"palabras_clave\":[\"3 a 5 keywords\"],\"slug_seo\":\"slug-seo-corto\",\"resumen_seo\":\"155 caracteres\"}\n";
-var MODELS_FREE = ["deepseek/deepseek-chat:free", "deepseek/deepseek-r1:free", "qwen/qwen-2.5-72b-instruct:free", "meta-llama/llama-3.3-70b-instruct:free"];
+
+// USA TU KEY - lee minuscula o mayuscula, la que tengas
+var getKey = function getKey() {
+  return process.env.openrouter_api_key || process.env.OPENROUTER_API_KEY || "";
+};
+var SYSTEM_PROMPT = "\nSos EDITOR JEFE de Ultravox Neuqu\xE9n, diario digital de Neuqu\xE9n capital.\n\nTAREA: Reescribir la noticia que te dan, manteniendo TODA la informaci\xF3n, sin resumir.\n\nREGLAS INQUEBRANTABLES - SI FALLAS UNA, FALLA TODO:\n\n1. LARGO PROPORCIONAL OBLIGATORIO:\n   - Cont\xE1 las palabras del texto original.\n   - Ten\xE9s que devolver EXACTAMENTE la misma cantidad o 10% m\xE1s.\n   - Si te mandan 200 palabras, devolv\xE9s 200. Si te mandan 800 palabras, devolv\xE9s 800.\n   - NUNCA resumas. Est\xE1 prohibido acortar.\n\n2. CERO ALUCINACI\xD3N:\n   - No inventes a\xF1os, fechas, montos, nombres, cargos, calles, organismos.\n   - Si el texto dice \"fin de a\xF1o\", dej\xE1 \"fin de a\xF1o\". No pongas 2024, 2025, 2026 si no est\xE1 escrito.\n   - Copi\xE1 nombres propios exactos como aparecen: funcionarios, calles, barrios, empresas.\n   - Copi\xE1 cifras exactas como aparecen: $30.000 millones, 70.000 veh\xEDculos, etc.\n\n3. CONSERVAR TODO:\n   - Manten\xE9 todas las declaraciones textuales entre comillas.\n   - Manten\xE9 todos los datos, todos los p\xE1rrafos, todos los subt\xEDtulos.\n   - Manten\xE9 el orden l\xF3gico de la noticia.\n\n4. FORMATO Y ESTILO:\n   - Us\xE1 HTML en contenido_mejorado: <p> para p\xE1rrafos (5 a 10 seg\xFAn el largo), <h2> para subt\xEDtulos si el original los tiene, <strong> para datos clave.\n   - Tono: Period\xEDstico profesional neuquino, calidad Clar\xEDn/La Naci\xF3n, objetivo, sin adjetivos militantes.\n   - T\xEDtulo: 60-75 caracteres, con verbo, atractivo.\n   - Bajada: 150-180 caracteres.\n\n5. SALIDA:\n   - Devolv\xE9 SOLO JSON v\xE1lido, sin texto antes ni despu\xE9s.\n   - Formato: {\"titulo\":\"\",\"bajada\":\"\",\"contenido_mejorado\":\"<p>...</p><h2>...</h2><p>...</p>\",\"palabras_clave\":[\"3 a 5\"],\"slug_seo\":\"slug-corto\",\"resumen_seo\":\"155 caracteres\"}\n\nSi no cumpl\xEDs con el largo proporcional, la respuesta ser\xE1 descartada.\n";
+var FREE_MODELS = ["deepseek/deepseek-chat:free", "deepseek/deepseek-r1:free", "qwen/qwen-2.5-72b-instruct:free", "meta-llama/llama-3.3-70b-instruct:free", "google/gemma-2-9b-it:free", "mistralai/mistral-7b-instruct:free"];
 function extractJSON(t) {
   var m = t.match(/\{[\s\S]*\}/);
-  if (!m) throw new Error("No JSON en respuesta");
+  if (!m) throw new Error("Sin JSON");
   return JSON.parse(m[0]);
 }
-function callOR(_x, _x2) {
-  return _callOR.apply(this, arguments);
+function callModel(_x, _x2, _x3) {
+  return _callModel.apply(this, arguments);
 }
-function _callOR() {
-  _callOR = _asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee2(model, prompt) {
+function _callModel() {
+  _callModel = _asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee2(model, prompt, apiKey) {
     var r;
     return _regenerator().w(function (_context2) {
       while (1) switch (_context2.n) {
@@ -48,11 +47,12 @@ function _callOR() {
             max_tokens: 4000
           }, {
             headers: {
-              "Authorization": "Bearer ".concat(process.env.OPENROUTER_API_KEY),
+              "Authorization": "Bearer ".concat(apiKey),
               "HTTP-Referer": "https://ultravox.com.ar",
-              "X-Title": "Ultravox"
+              "X-Title": "Ultravox",
+              "Content-Type": "application/json"
             },
-            timeout: 45000
+            timeout: 40000
           });
         case 1:
           r = _context2.v;
@@ -60,93 +60,135 @@ function _callOR() {
       }
     }, _callee2);
   }));
-  return _callOR.apply(this, arguments);
+  return _callModel.apply(this, arguments);
 }
+router.get("/", function (req, res) {
+  var k = getKey();
+  res.json({
+    ok: true,
+    endpoint: "/api/v2/vox-rewrite V6 FULL",
+    has_key: !!k,
+    key_var: k ? "tu openrouter_api_key OK" : "NO KEY"
+  });
+});
 router.post("/", /*#__PURE__*/function () {
   var _ref = _asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee(req, res) {
-    var original, wordCount, contenido, prompt, _i, _MODELS_FREE, model, data, outWords, _e$response, _t, _t2;
+    var API_KEY, original, wordCount, contenido, prompt, errors, _i, _FREE_MODELS, model, data, outText, outWords, _e$response, msg, _e$response2, _t, _t2;
     return _regenerator().w(function (_context) {
       while (1) switch (_context.p = _context.n) {
         case 0:
-          _context.p = 0;
+          API_KEY = getKey();
+          if (API_KEY) {
+            _context.n = 1;
+            break;
+          }
+          return _context.a(2, res.status(500).json({
+            ok: false,
+            error: "missing_api_key",
+            detail: "No se encontró openrouter_api_key en Koyeb env vars"
+          }));
+        case 1:
+          _context.p = 1;
           original = req.body.texto_original || req.body.contenido || "";
           if (!(!original || original.length < 50)) {
-            _context.n = 1;
+            _context.n = 2;
             break;
           }
           return _context.a(2, res.status(400).json({
             ok: false,
-            error: "Texto muy corto"
+            error: "texto_original vacio"
           }));
-        case 1:
+        case 2:
           wordCount = original.trim().split(/\s+/).length;
           contenido = original.slice(0, 8000);
-          prompt = "TEXTO ORIGINAL: ".concat(wordCount, " palabras. Ten\xE9s que devolver ").concat(wordCount, " palabras aprox, no menos de ").concat(Math.floor(wordCount * 0.9), ". No resumas, reescrib\xED completo conservando todo.\n\n").concat(contenido, "\n\nDevolv\xE9 JSON largo ahora.");
-          _i = 0, _MODELS_FREE = MODELS_FREE;
-        case 2:
-          if (!(_i < _MODELS_FREE.length)) {
-            _context.n = 8;
+          prompt = "\nTEXTO ORIGINAL: ".concat(wordCount, " palabras.\nINSTRUCCI\xD3N: Ten\xE9s que devolver ").concat(wordCount, " palabras m\xEDnimo (no menos de ").concat(Math.floor(wordCount * 0.9), "). Es OBLIGATORIO que sea largo proporcional. No resumas. Reescrib\xED completo conservando todos los p\xE1rrafos, datos y declaraciones.\n\n").concat(contenido, "\n\nDevolv\xE9 JSON largo ahora.\n");
+          errors = [];
+          _i = 0, _FREE_MODELS = FREE_MODELS;
+        case 3:
+          if (!(_i < _FREE_MODELS.length)) {
+            _context.n = 10;
             break;
           }
-          model = _MODELS_FREE[_i];
-          _context.p = 3;
-          console.log("[vox] IN: ".concat(wordCount, " palabras -> ").concat(model));
-          _context.n = 4;
-          return callOR(model, prompt);
-        case 4:
-          data = _context.v;
-          outWords = (data.contenido_mejorado || "").replace(/<[^>]*>/g, " ").split(/\s+/).filter(Boolean).length;
-          console.log("[vox] OUT: ".concat(outWords, " palabras"));
-
-          // Validación proporcional genérica
-          if (!(outWords < wordCount * 0.7)) {
-            _context.n = 5;
-            break;
-          }
-          throw new Error("Resumen detectado: ".concat(outWords, " palabras vs ").concat(wordCount, " originales. Reintento."));
+          model = _FREE_MODELS[_i];
+          _context.p = 4;
+          console.log("[vox] IN:".concat(wordCount, " -> ").concat(model));
+          _context.n = 5;
+          return callModel(model, prompt, API_KEY);
         case 5:
-          return _context.a(2, res.json(_objectSpread(_objectSpread({
+          data = _context.v;
+          outText = (data.contenido_mejorado || "").replace(/<[^>]*>/g, " ");
+          outWords = outText.split(/\s+/).filter(Boolean).length;
+          console.log("[vox] OUT:".concat(outWords, " palabras"));
+
+          // Validación proporcional
+          if (!(outWords < wordCount * 0.7)) {
+            _context.n = 6;
+            break;
+          }
+          throw new Error("Resumen detectado: ".concat(outWords, " vs ").concat(wordCount, " originales"));
+        case 6:
+          return _context.a(2, res.json({
             ok: true,
             model: model,
             stats: {
               "in": wordCount,
               out: outWords
-            }
-          }, data), {}, {
+            },
+            titulo: data.titulo,
+            bajada: data.bajada,
+            contenido_mejorado: data.contenido_mejorado,
+            palabras_clave: data.palabras_clave,
+            slug_seo: data.slug_seo,
+            resumen_seo: data.resumen_seo,
             data: {
               Entry_Title: data.titulo,
               Entry_Bajada: data.bajada,
               Entry_Content: data.contenido_mejorado
             }
-          })));
-        case 6:
-          _context.p = 6;
-          _t = _context.v;
-          console.log("[vox] Fall\xF3 ".concat(model, ": ").concat(_t.message));
-          return _context.a(3, 7);
+          }));
         case 7:
-          _i++;
-          _context.n = 2;
-          break;
+          _context.p = 7;
+          _t = _context.v;
+          msg = ((_e$response = _t.response) === null || _e$response === void 0 || (_e$response = _e$response.data) === null || _e$response === void 0 || (_e$response = _e$response.error) === null || _e$response === void 0 ? void 0 : _e$response.message) || _t.message;
+          console.log("[vox] Fail ".concat(model, ": ").concat(msg));
+          errors.push({
+            model: model,
+            error: msg
+          });
+          if (!msg.includes("429")) {
+            _context.n = 8;
+            break;
+          }
+          _context.n = 8;
+          return new Promise(function (r) {
+            return setTimeout(r, 1500);
+          });
         case 8:
+          return _context.a(3, 9);
+        case 9:
+          _i++;
+          _context.n = 3;
+          break;
+        case 10:
           return _context.a(2, res.status(500).json({
             ok: false,
-            error: "all_free_models_failed"
+            error: "all_models_saturated",
+            errors: errors
           }));
-        case 9:
-          _context.p = 9;
+        case 11:
+          _context.p = 11;
           _t2 = _context.v;
-          console.error(((_e$response = _t2.response) === null || _e$response === void 0 ? void 0 : _e$response.data) || _t2.message);
+          console.error("[vox] fatal", ((_e$response2 = _t2.response) === null || _e$response2 === void 0 ? void 0 : _e$response2.data) || _t2.message);
           res.status(500).json({
             ok: false,
             error: _t2.message
           });
-        case 10:
+        case 12:
           return _context.a(2);
       }
-    }, _callee, null, [[3, 6], [0, 9]]);
+    }, _callee, null, [[4, 7], [1, 11]]);
   }));
-  return function (_x3, _x4) {
+  return function (_x4, _x5) {
     return _ref.apply(this, arguments);
   };
 }());
--- src/routes/v2/vox-rewrite.routes.js
+++ src/routes/v2/vox-rewrite.routes.js
@@ -2,103 +2,155 @@ import express from "express";
 import axios from "axios";
 const router = express.Router();
 
+// USA TU KEY - lee minuscula o mayuscula, la que tengas
+const getKey = () => process.env.openrouter_api_key || process.env.OPENROUTER_API_KEY || "";
+
 const SYSTEM_PROMPT = `
-Sos EDITOR JEFE de un diario digital argentino.
+Sos EDITOR JEFE de Ultravox Neuquén, diario digital de Neuquén capital.
 
-TAREA: Reescribir la noticia que te dan, manteniendo TODA la información.
+TAREA: Reescribir la noticia que te dan, manteniendo TODA la información, sin resumir.
 
-REGLAS OBLIGATORIAS:
-1. LARGO PROPORCIONAL: Contá las palabras del texto original. Devolvé la MISMA cantidad de palabras o un poco más (90% a 110%). Si el original tiene 200 palabras, devolvés 200. Si tiene 800, devolvés 800. NUNCA resumas.
-2. PROHIBIDO INVENTAR: No inventes fechas, años, nombres, montos, lugares, cargos, estadísticas. Copiá exactamente lo que dice el texto original. Si dice "fin de año", dejá "fin de año". No agregues el año si no está.
-3. CONSERVAR TODO: Mantené todas las declaraciones textuales, todos los nombres propios, todos los números, todas las calles/barrios/organismos.
-4. FORMATO: Usá HTML para contenido_mejorado: <p> para párrafos, <h2> para cada subtítulo que ya exista en el original, <strong> para datos clave. 5 a 10 párrafos según el largo original.
-5. Tono: Profesional, objetivo, argentino.
+REGLAS INQUEBRANTABLES - SI FALLAS UNA, FALLA TODO:
 
-Devolvé SOLO JSON válido:
-{"titulo":"60-75 caracteres, atractivo","bajada":"150-180 caracteres","contenido_mejorado":"HTML largo y proporcional","palabras_clave":["3 a 5 keywords"],"slug_seo":"slug-seo-corto","resumen_seo":"155 caracteres"}
+1. LARGO PROPORCIONAL OBLIGATORIO:
+   - Contá las palabras del texto original.
+   - Tenés que devolver EXACTAMENTE la misma cantidad o 10% más.
+   - Si te mandan 200 palabras, devolvés 200. Si te mandan 800 palabras, devolvés 800.
+   - NUNCA resumas. Está prohibido acortar.
+
+2. CERO ALUCINACIÓN:
+   - No inventes años, fechas, montos, nombres, cargos, calles, organismos.
+   - Si el texto dice "fin de año", dejá "fin de año". No pongas 2024, 2025, 2026 si no está escrito.
+   - Copiá nombres propios exactos como aparecen: funcionarios, calles, barrios, empresas.
+   - Copiá cifras exactas como aparecen: $30.000 millones, 70.000 vehículos, etc.
+
+3. CONSERVAR TODO:
+   - Mantené todas las declaraciones textuales entre comillas.
+   - Mantené todos los datos, todos los párrafos, todos los subtítulos.
+   - Mantené el orden lógico de la noticia.
+
+4. FORMATO Y ESTILO:
+   - Usá HTML en contenido_mejorado: <p> para párrafos (5 a 10 según el largo), <h2> para subtítulos si el original los tiene, <strong> para datos clave.
+   - Tono: Periodístico profesional neuquino, calidad Clarín/La Nación, objetivo, sin adjetivos militantes.
+   - Título: 60-75 caracteres, con verbo, atractivo.
+   - Bajada: 150-180 caracteres.
+
+5. SALIDA:
+   - Devolvé SOLO JSON válido, sin texto antes ni después.
+   - Formato: {"titulo":"","bajada":"","contenido_mejorado":"<p>...</p><h2>...</h2><p>...</p>","palabras_clave":["3 a 5"],"slug_seo":"slug-corto","resumen_seo":"155 caracteres"}
+
+Si no cumplís con el largo proporcional, la respuesta será descartada.
 `;
 
-const MODELS_FREE = [
+const FREE_MODELS = [
   "deepseek/deepseek-chat:free",
   "deepseek/deepseek-r1:free",
   "qwen/qwen-2.5-72b-instruct:free",
-  "meta-llama/llama-3.3-70b-instruct:free"
+  "meta-llama/llama-3.3-70b-instruct:free",
+  "google/gemma-2-9b-it:free",
+  "mistralai/mistral-7b-instruct:free"
 ];
 
 function extractJSON(t){
   const m = t.match(/\{[\s\S]*\}/);
-  if(!m) throw new Error("No JSON en respuesta");
+  if(!m) throw new Error("Sin JSON");
   return JSON.parse(m[0]);
 }
 
-async function callOR(model, prompt){
+async function callModel(model, prompt, apiKey){
   const r = await axios.post("https://openrouter.ai/api/v1/chat/completions", {
     model,
     messages: [
-      {role:"system", content: SYSTEM_PROMPT},
-      {role:"user", content: prompt}
+      { role:"system", content: SYSTEM_PROMPT },
+      { role:"user", content: prompt }
     ],
     temperature: 0.15,
     max_tokens: 4000
   },{
-    headers: {
-      "Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`,
+    headers:{
+      "Authorization": `Bearer ${apiKey}`,
       "HTTP-Referer": "https://ultravox.com.ar",
-      "X-Title": "Ultravox"
+      "X-Title": "Ultravox",
+      "Content-Type":"application/json"
     },
-    timeout: 45000
+    timeout: 40000
   });
   return extractJSON(r.data.choices[0].message.content);
 }
 
+router.get("/", (req,res)=>{
+  const k = getKey();
+  res.json({ ok:true, endpoint:"/api/v2/vox-rewrite V6 FULL", has_key:!!k, key_var: k? "tu openrouter_api_key OK" : "NO KEY" });
+});
+
 router.post("/", async (req,res)=>{
+  const API_KEY = getKey();
+  if(!API_KEY) return res.status(500).json({ ok:false, error:"missing_api_key", detail:"No se encontró openrouter_api_key en Koyeb env vars" });
+
   try{
     const original = req.body.texto_original || req.body.contenido || "";
-    if(!original || original.length < 50) return res.status(400).json({ok:false, error:"Texto muy corto"});
+    if(!original || original.length < 50) return res.status(400).json({ ok:false, error:"texto_original vacio" });
 
     const wordCount = original.trim().split(/\s+/).length;
     const contenido = original.slice(0, 8000);
 
-    const prompt = `TEXTO ORIGINAL: ${wordCount} palabras. Tenés que devolver ${wordCount} palabras aprox, no menos de ${Math.floor(wordCount*0.9)}. No resumas, reescribí completo conservando todo.
+    const prompt = `
+TEXTO ORIGINAL: ${wordCount} palabras.
+INSTRUCCIÓN: Tenés que devolver ${wordCount} palabras mínimo (no menos de ${Math.floor(wordCount*0.9)}). Es OBLIGATORIO que sea largo proporcional. No resumas. Reescribí completo conservando todos los párrafos, datos y declaraciones.
 
 ${contenido}
 
-Devolvé JSON largo ahora.`;
+Devolvé JSON largo ahora.
+`;
 
-    for(const model of MODELS_FREE){
+    const errors = [];
+
+    for(const model of FREE_MODELS){
       try{
-        console.log(`[vox] IN: ${wordCount} palabras -> ${model}`);
-        const data = await callOR(model, prompt);
+        console.log(`[vox] IN:${wordCount} -> ${model}`);
+        const data = await callModel(model, prompt, API_KEY);
 
-        const outWords = (data.contenido_mejorado || "").replace(/<[^>]*>/g," ").split(/\s+/).filter(Boolean).length;
-        console.log(`[vox] OUT: ${outWords} palabras`);
+        const outText = (data.contenido_mejorado||"").replace(/<[^>]*>/g," ");
+        const outWords = outText.split(/\s+/).filter(Boolean).length;
 
-        // Validación proporcional genérica
+        console.log(`[vox] OUT:${outWords} palabras`);
+
+        // Validación proporcional
         if(outWords < wordCount * 0.7){
-          throw new Error(`Resumen detectado: ${outWords} palabras vs ${wordCount} originales. Reintento.`);
+          throw new Error(`Resumen detectado: ${outWords} vs ${wordCount} originales`);
         }
 
         return res.json({
-          ok: true,
+          ok:true,
           model,
-          stats: { in: wordCount, out: outWords },
-         ...data,
-          data: {
+          stats:{ in: wordCount, out: outWords },
+          titulo: data.titulo,
+          bajada: data.bajada,
+          contenido_mejorado: data.contenido_mejorado,
+          palabras_clave: data.palabras_clave,
+          slug_seo: data.slug_seo,
+          resumen_seo: data.resumen_seo,
+          data:{
             Entry_Title: data.titulo,
             Entry_Bajada: data.bajada,
             Entry_Content: data.contenido_mejorado
           }
         });
+
       }catch(e){
-        console.log(`[vox] Falló ${model}: ${e.message}`);
+        const msg = e.response?.data?.error?.message || e.message;
+        console.log(`[vox] Fail ${model}: ${msg}`);
+        errors.push({ model, error: msg });
+        if(msg.includes("429")) await new Promise(r=>setTimeout(r,1500));
         continue;
       }
     }
 
-    return res.status(500).json({ok:false, error:"all_free_models_failed"});
+    return res.status(500).json({ ok:false, error:"all_models_saturated", errors });
+
   }catch(e){
-    console.error(e.response?.data || e.message);
-    res.status(500).json({ok:false, error:e.message});
+    console.error("[vox] fatal", e.response?.data || e.message);
+    res.status(500).json({ ok:false, error:e.message });
   }
 });
 
